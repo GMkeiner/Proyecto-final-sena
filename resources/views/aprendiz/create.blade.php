a764936c69324ac110b144d209c3fc36d35560d2
Gmkeiner <munozpallareskeinerjose@gmail.com>
2024-09-04 16:01:07 -0500

    Nuevos ajustes en el diseño

--- resources/views/aprendiz/create.blade.php
+++ resources/views/aprendiz/create.blade.php
@@ -1,9 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/stylesUnico.css') }}">
     <title>Document</title>
 </head>
 <body>
@@ -55,11 +59,12 @@
                        <option value="{{$fichas->id }}">{{$fichas->noFicha}}</option>"
                        @endforeach
                     </select>
-                </div>
-              <a href="{{ url('aprendiz') }}"  class="btn btn-secondary">Regresar</a>
+                    <a href="{{ url('aprendiz') }}"  class="btn btn-secondary">Regresar</a>
               <button type="sumit" class="btn btn-success">Guardar</button>
+                </div>
              </div>
     </form>
     </div>
 </body>
 </html>
+@endsection
--- resources/views/competencias/create.blade.php
+++ resources/views/competencias/create.blade.php
@@ -7,11 +7,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/stylesUnico.css') }}">
         <title>Document</title>
     </head>
     <body>
         <div class="container py-4">
-            <h2>Registrar COmpetencia</h2>
+            <h2>Registrar Competencia</h2>
 
             <form action="{{ url('competencias') }}" method="post">
 
@@ -20,10 +21,10 @@
                     <label for="nombre" class="col-sm-2 col-form-label">Nombre de la competencia:</label>
                     <div class="col-sm-5">
                         <input type="text" class="form-control"  name="nombre"  id="nombre" value="{{old('nombre')}}" required>
-                    </div>
-                <a href="{{ url('competencias') }}"  class="btn btn-secondary">Regresar</a>
+                        <a href="{{ url('competencias') }}"  class="btn btn-secondary">Regresar</a>
                 <button type="sumit" class="btn btn-success">Guardar</button>
                 </div>
+                    </div>
         </form>
         </div>
 
