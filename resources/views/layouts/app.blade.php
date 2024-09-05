216d8a6e03ff153b98e15681b7c9af1891cdcb78
ldavid23 <ldavidjb05@gmail.com>
2024-09-05 09:16:06 -0500

    Recuperando lo que se perdió en el merge

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     {{-- <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet"> --}}
     <link rel="stylesheet" href="{{asset('assets/css/styles1.css')}}">
     @role('Instructor')<link rel="stylesheet" href="{{asset('assets/css/stylesNotas.css')}}">@endrole
+    @role('Admin')<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/stylesUnico.css') }}">@endrole
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
