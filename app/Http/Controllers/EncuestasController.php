0b80dccd077120b4cd1f5cc3bbcb7a050074b3db
Gmkeiner <munozpallareskeinerjose@gmail.com>
2024-09-16 16:35:43 -0500

    Merge on the branch

--- app/Http/Controllers/EncuestasController.php
+++ app/Http/Controllers/EncuestasController.php
@@ -33,6 +33,7 @@ class EncuestasController extends Controller
     public function store(Request $request)
     {
         // Validaciones para la encuesta
+        // no tocar el codigo
         $validated = $request->validate([
             'aprendiz_id' => 'required|exists:aprendizs,user_id',
             'respuesta1' => 'required|integer|between:1,5',
